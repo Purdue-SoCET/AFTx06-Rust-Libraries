c2cb53f3a455a1287ab6cc12c68bfaf7277b70a8
goldenbergg <ggoldenb@purdue.edu>
2021-07-19 13:59:38 -0400

    Added enumerations for PWM channels in pwm.rs.

--- src/apb/pwm.rs
+++ src/apb/pwm.rs
@@ -19,6 +19,10 @@ pub const PWM_CHANNEL_SIZE: u32 =         0x0C;
 pub const PWM_MAX_FREQ: u32 =             common::CHIP_FREQ / 2;
 pub const AFTX06_DUTY_OFFSET: u32 =       1;
 
+pub enum Channel {
+    CH0,
+}
+
 pub struct PWM {
     p: &'static mut PWMRegisterBlock
 }
@@ -45,112 +49,103 @@ impl PWM {
         }
     }
 
-    pub fn set_frequency(&mut self, channel: u32, mut frequency: u32) {
+    pub fn set_frequency(&mut self, channel: Channel, mut frequency: u32) {
         unsafe {
             if PWM_MAX_FREQ < frequency { frequency = PWM_MAX_FREQ; }
             let period: u32 = common::rounding_division(common::CHIP_FREQ, frequency);
             match channel {
-                0 => {
+                Channel::CH0 => {
                     self.p.pwm0_period.write(period);
                     self.p.pwm0_duty.write(common::rounding_division(period, 2) + AFTX06_DUTY_OFFSET);
                 }
-                _ => panic!("You may only write to pwm channel 0."),
             }
         }
     }
 
-    pub fn set_period(&mut self, channel: u32, period: u32) {
+    pub fn set_period(&mut self, channel: Channel, period: u32) {
         unsafe {
             match channel {
-                0 => self.p.pwm0_period.write(period),
-                _ => panic!("You may only write to pwm channel 0."),
+                Channel::CH0 => self.p.pwm0_period.write(period),
             }
         }
     }
 
-    pub fn set_duty(&mut self, channel: u32, duty: u32) {
+    pub fn set_duty(&mut self, channel: Channel, duty: u32) {
         unsafe {
             match channel {
-                0 => self.p.pwm0_duty.write(duty + AFTX06_DUTY_OFFSET),
-                _ => panic!("You may only write to pwm channel 0."),
+                Channel::CH0 => self.p.pwm0_duty.write(duty + AFTX06_DUTY_OFFSET),
             }
         }
     }
 
-    pub fn disable(&mut self, channel: u32) {
+    pub fn disable(&mut self, channel: Channel) {
         unsafe {
             match channel {
-                0 => {
+                Channel::CH0 => {
                     let mut curr: u32 = self.p.pwm0_ctrl.read();
                     curr &= PWM_CONTROL_DISABLE;
                     self.p.pwm0_ctrl.write(curr);
                 }
-                _ => panic!("You may only write to pwm channel 0."),
             }
         }
     }
 
-    pub fn enable(&mut self, channel: u32) {
+    pub fn enable(&mut self, channel: Channel) {
         unsafe {
             match channel {
-                0 => {
+                Channel::CH0 => {
                     let mut curr: u32 = self.p.pwm0_ctrl.read();
                     curr |= PWM_CONTROL_ENABLE;
                     self.p.pwm0_ctrl.write(curr);
                 }
-                _ => panic!("You may only write to pwm channel 0."),
             }
         }
     }
 
-    pub fn set_active_high(&mut self, channel: u32) {
+    pub fn set_active_high(&mut self, channel: Channel) {
         unsafe {
             match channel {
-                0 => {
+                Channel::CH0 => {
                     let mut curr: u32 = self.p.pwm0_ctrl.read();
                     curr &= PWM_CONTROL_ACTIVE_HIGH;
                     self.p.pwm0_ctrl.write(curr);
                 }
-                _ => panic!("You may only write to pwm channel 0."),
             }
         }
     }
 
-    pub fn set_active_low(&mut self, channel: u32) {
+    pub fn set_active_low(&mut self, channel: Channel) {
         unsafe {
             match channel {
-                0 => {
+                Channel::CH0 => {
                     let mut curr: u32 = self.p.pwm0_ctrl.read();
                     curr |= PWM_CONTROL_ACTIVE_LOW;
                     self.p.pwm0_ctrl.write(curr);
                 }
-                _ => panic!("You may only write to pwm channel 0."),
             }
         }
     }
 
-    pub fn set_align_left(&mut self, channel: u32) {
+    pub fn set_align_left(&mut self, channel: Channel) {
         unsafe {
             match channel {
-                0 => {
+                Channel::CH0 => {
                     let mut curr: u32 = self.p.pwm0_ctrl.read();
                     curr &= PWM_CONTROL_ALIGN_LEFT;
                     self.p.pwm0_ctrl.write(curr);
                 }
-                _ => panic!("You may only write to pwm channel 0."),
             }
         }
     }
 
-    pub fn set_align_center(&mut self, channel: u32) {
+    pub fn set_align_center(&mut self, channel: Channel) {
         unsafe {
             match channel {
-                0 => {
+                Channel::CH0 => {
                     let mut curr: u32 = self.p.pwm0_ctrl.read();
                     curr |= PWM_CONTROL_ALIGN_CENTER;
                     self.p.pwm0_ctrl.write(curr);
                 }
-                _ => panic!("You may only write to pwm channel 0."),
             }
         }
     }
